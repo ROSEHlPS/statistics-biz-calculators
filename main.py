8684207957fbb5c7bb2486f3dec09b095b7c2268
cas <90588628+ROSEHlPS@users.noreply.github.com>
2021-10-03 11:27:21 -0700

    Update main.py
    Duplicate of my stats calculator (I'm new to the Github interface :))

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #per 30 days
 #program prints standard deviations
 
-#daily
-
-#sales per day, each interval is one day of the month
-sales = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+#NUMBER OF SALES PER DAY
+entered_sales = input("Enter each sale per day, separated by a space: ").split()
+sales = list(map(int,entered_sales))
+#sales = [2, 10, 9, 4, 8, 1, 8, 7, 9, 6, 9, 5, 5]
 
 #sales
 #sales and orders
@@ -12,17 +12,20 @@ def print_sales(sales_input):
   for sale in sales_input:
     print (sale)
 
-def sales_sum(orders):
-  total = 0
-  for order in orders: 
-    total += order
-  return total
+#def sales_sum(orders):
+  #total = 0
+  #for order in orders: 
+    #total += order
+  #return total
     
+sales_sum = (sum(sales))
+print ("Total number of sales made = ", sales_sum)
+
 def sales_average(sales_input):
-  sum_of_sales = sales_sum(sales_input)
-  average = sum_of_sales / float(len(sales_input))
+  average = sales_sum / float(len(sales_input))
   return average
 
+print ("Average number of sales: ", sales_average(sales))
 
 #how sales varied against the average
 #large variance- differed
@@ -35,49 +38,51 @@ def sales_variance(orders):
       variance += (average - order) ** 2
   return variance / len(orders)
 
+print ("Variance of sales: ", sales_variance(sales))
+
 def sales_std_deviation(variance):
   return variance ** 0.5
 
 variance = sales_variance(sales)
 
+print ("Standard deviation of number of sales: ", sales_std_deviation(variance))
 
-print (sales_sum(sales))
-print (sales_average(sales))
-print (sales_variance(sales))
-print (sales_std_deviation(variance))
+#print (sales_sum(sales))
+#print (sales_average(sales))
+#print (sales_variance(sales))
+
 
 #------------------------------
+print ("------")
 
-#change variables so it fits the shop
+#VALUE OF EACH SALE'S TOTAL $$
 
-#profit, earnings
-#revenue
-
-
-#net income
-#change variables
-#sale--> revenue (amount made per sale)
-#scores--> profit (individual sale amount)
-
-#amount totaled revenue per day of the month
-
-revenue = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+entered_revenue = input("Enter the amount made per sale, separated by a space: ").split()
+revenue = list(map(int,entered_revenue))
 
 #sales
-def print_revenues(revenues_input):
-  for revenue in revenues_input:
+def print_revenues(revenue_input):
+  for revenue in revenue_input:
     print (revenue)
 
-def revenues_sum(profits):
+
+def revenue_sum(profits):
   total = 0
   for profit in profits: 
     total += profit
   return total
     
-def revenues_average(revenues_input):
-  sum_of_revenues = revenues_sum(revenues_input)
-  average = sum_of_revenues / float(len(revenues_input))
+
+print ("Total sum of profit made: ", revenue_sum(revenue))
+
+
+def revenues_average(revenue_input):
+  sum_of_revenues = revenue_sum(revenue_input)
+  average = sum_of_revenues / float(len(revenue_input))
   return average
+
+
+print ("Average profit made per day: ", (revenues_average(revenue)))
 
 def revenues_variance(profits):
   average = revenues_average(profits)
@@ -86,22 +91,19 @@ def revenues_variance(profits):
       variance += (average - profit) ** 2
   return variance / len(profits)
 
-
-def revenues_std_deviation(variance):
-  return variance ** 0.5
-
-variance = revenues_variance(revenue)
+print ("Revenue variance: ", revenues_variance(revenue))
 
 
-print (revenues_sum(revenue))
-print (revenues_average(revenue))
-print (revenues_variance(revenue))
-print (revenues_std_deviation(variance))
 
-#-------------------------------
+#print (revenues_sum(revenue))
+#print (revenues_average(revenue))
+#print (revenues_variance(revenue))
+#print (revenues_std_deviation(variance))
+
+print ("-------------------------------")
 #goal reevenue calculator
 goal_profit = float(input("Enter Goal for this Month: "))
-actual_profit = (revenues_sum(revenue))
+actual_profit = (revenue_sum(revenue))
 
 if (goal_profit > actual_profit):
   revenue = goal_profit - actual_profit
